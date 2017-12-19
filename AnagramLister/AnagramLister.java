54b0ac25fd5a627a26b0f40ea32b08437a1284b9
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-12-19 03:11:40 -0700

    implemented AnagramLister, but errors still to be fixed

--- AnagramLister/AnagramLister.java
+++ AnagramLister/AnagramLister.java
@@ -1,5 +1,5 @@
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -35,22 +35,52 @@ public class AnagramLister	{
 	}
 
 
-		private Text sortedWordKey = new Text();	// declare the key
-		private final static IntWritable oneValue = new IntWritable(1);		// define the value
+	public static class AnagramListerReducer extends Reducer<Text, Text, Text, Text>	{
+		private Text sortedWordKey = new Text();
+		private Text correspondingAnagramsValue = new Text();
 
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException	{
-			StringTokenizer token = new StringTokenizer(value.toString());
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException	{
+			HashSet<String> hashset = new HashSet<String>();
+			Iterator<String> iterator = hashset.iterator();
+			String resultString = "";
+			int count = 0;
 
-			while(token.hasMoreTokens())	{
-				String tempWord = value.toString();
-				char[] tempWordCharArray = tempWord.toCharArray();
-				Arrays.sort(tempWordCharArray);
-				String sortedTempWord = new String(tempWordCharArray);
-				System.out.println(sortedTempWord);
-				sortedWordKey.set(sortedTempWord);		// set the key to the sorted word
-
-				context.write(sortedWordKey, oneValue);	// write key value pair to context
+			for(Text val : values)	{
+				String temp = val.toString();	// not val.get(). IntWritables have val.get()
+				hashset.add(temp);
 			}
+
+			while(iterator.hasNext())	{
+				resultString = resultString + iterator.next() + ", ";
+				count++;
+			}
+
+			if(count >= 2)	{
+				sortedWordKey.set(key);
+				correspondingAnagramsValue.set(resultString);
+
+				context.write(sortedWordKey, correspondingAnagramsValue);
+			}
+
 		}
+	}
+
+
+	public static void main(String[] args) throws Exception {
+	    Configuration conf = new Configuration();       // define a configuration
+	    Job job = Job.getInstance(conf, "word count");  // define a job
+	    job.setJarByClass(AnagramLister.class);
+	    job.setMapperClass(AnagramListerMapper.class);          // set the mapper to the job
+	    job.setCombinerClass(AnagramListerReducer.class);       // does local aggregation
+	    job.setReducerClass(AnagramListerReducer.class);        // set the reducer to the job
+	    job.setOutputKeyClass(Text.class);              // type (i.e. class) of output key
+	    job.setOutputValueClass(IntWritable.class);     // type (i.e. class) of output value
+	    job.setMapOutputKeyClass(Text.class);
+	    job.setMapOutputValueClass(Text.class);
+
+	    FileInputFormat.addInputPath(job, new Path(args[0]));   // input path specification
+	    FileOutputFormat.setOutputPath(job, new Path(args[1])); // output path specification
+	    
+	    System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
