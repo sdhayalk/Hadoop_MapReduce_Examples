62651dfb0ad498e179c0c7d2fa0f6338682dd6f2
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-12-19 22:52:16 -0700

    anagram lister completed and tested

--- AnagramLister/AnagramLister.java
+++ AnagramLister/AnagramLister.java
@@ -36,13 +36,11 @@ public class AnagramLister	{
 
 
 	public static class AnagramListerReducer extends Reducer<Text, Text, Text, Text>	{
-		private Text sortedWordKey = new Text();
-		private Text correspondingAnagramsValue = new Text();
+		private Text correspondingAnagramsValue;
 
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException	{
 			HashSet<String> hashset = new HashSet<String>();
 			String resultString = "";
-			int count = 0;
 
 			for(Text val : values)	{
 				String temp = val.toString();	// not val.get(). IntWritables have val.get()
@@ -52,15 +50,13 @@ public class AnagramLister	{
 			Iterator<String> iterator = hashset.iterator();
 			while(iterator.hasNext())	{
 				String temp = iterator.next();
-				resultString = resultString + temp + ", ";
-				count++;
+				resultString = resultString + temp + ",";
 			}
 
-			if(count >= 2)	{
-				sortedWordKey.set(key);
-				correspondingAnagramsValue.set(resultString);
-
-				context.write(sortedWordKey, correspondingAnagramsValue);
+			StringTokenizer token = new StringTokenizer(resultString,",");
+			if(token.countTokens() >= 2)	{
+				correspondingAnagramsValue = new Text(resultString);
+				context.write(key, correspondingAnagramsValue);
 			}
 
 		}
@@ -69,13 +65,13 @@ public class AnagramLister	{
 
 	public static void main(String[] args) throws Exception {
 	    Configuration conf = new Configuration();       // define a configuration
-	    Job job = Job.getInstance(conf, "word count");  // define a job
+	    Job job = Job.getInstance(conf, "anagram lister");  // define a job
 	    job.setJarByClass(AnagramLister.class);
 	    job.setMapperClass(AnagramListerMapper.class);          // set the mapper to the job
 	    job.setCombinerClass(AnagramListerReducer.class);       // does local aggregation
 	    job.setReducerClass(AnagramListerReducer.class);        // set the reducer to the job
 	    job.setOutputKeyClass(Text.class);              // type (i.e. class) of output key
-	    job.setOutputValueClass(IntWritable.class);     // type (i.e. class) of output value
+	    job.setOutputValueClass(Text.class);     		// type (i.e. class) of output value
 	    job.setMapOutputKeyClass(Text.class);
 	    job.setMapOutputValueClass(Text.class);
 
